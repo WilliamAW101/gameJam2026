50afb34cd9cf99bd82fc65cf12745abf0c7012ee
Kai Sebastian-Peck <peckmaster824@gmail.com>
2026-01-17 10:51:07 -0500

    Planet Screen Minor Changes
    Small changes

--- ExplorationJam/Assets/Camera Screen/Planets/PlanetBehavior.cs
+++ ExplorationJam/Assets/Camera Screen/Planets/PlanetBehavior.cs
@@ -4,15 +4,35 @@ public class PlanetBehavior : MonoBehaviour
 {
     public GameObject findableObject;
 
+    public SphereCollider SphereCollider;
+    public float radius;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        SphereCollider = GetComponent<SphereCollider>();
+
+        radius = SphereCollider.radius;
+
+        SpawnOnPlanet();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnOnPlanet()
+    {
+        
+        Vector3 randomDir = Random.onUnitSphere;
+
+        Vector3 spawnPosition = transform.position + randomDir * radius;
+
+        GameObject foinstance = Instantiate(findableObject, spawnPosition, Quaternion.FromToRotation(Vector3.forward, randomDir));
+        transform.SetParent
+        foinstance.transform.localScale = findableObject.transform.localScale;
+        foinstance.SetActive(true);
     }
 }
