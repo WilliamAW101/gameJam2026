b38ef703027700d86cdd06d746aa751d1c310c23
Kai Sebastian-Peck <peckmaster824@gmail.com>
2026-01-18 15:13:33 -0500

    Fixed update changes

--- ExplorationJam/Assets/Map/Scripts/SatelliteControl.cs
+++ ExplorationJam/Assets/Map/Scripts/SatelliteControl.cs
@@ -15,8 +15,27 @@ public class SatelliteControl : MonoBehaviour
     {
         satelliteThruster = GetComponent<Rigidbody>();
     }
-    
-    void Update()
+
+    private void Update()
+    {
+        if (orbitplanet.getIsAPlanet() == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Debug.Log(orbitplanet.getCurrentPlanetID());
+
+                if (Transitions.Instance != null)
+                {
+                    Transitions.Instance.ToCameraTransition(orbitplanet.getCurrentPlanetID());
+                }
+                else
+                {
+                    Debug.LogError("Bloody instance is null");
+                }
+            }
+        }
+    }
+    void FixedUpdate()
     {
         rotateSatellite();
 
@@ -43,23 +62,6 @@ public class SatelliteControl : MonoBehaviour
             else
             {
                 Debug.LogError("Resource Tracker instance is null!");
-            }
-        }
-        
-        if (orbitplanet.getIsAPlanet() == true)
-        {
-            if (Input.GetKey(KeyCode.Space))
-            {
-                Debug.Log(orbitplanet.getCurrentPlanetID());
-                
-                if (Transitions.Instance != null)
-                {
-                    Transitions.Instance.ToCameraTransition(orbitplanet.getCurrentPlanetID());
-                }
-                else
-                {
-                    Debug.LogError("Bloody instance is null");
-                }
             }
         }
     }
