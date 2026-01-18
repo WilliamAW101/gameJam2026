38ec825c0ed8e42e8525b9c731e4fcf0b3cc6141
Jzap96 <Joshzappms@gmail.com>
2026-01-18 02:48:36 -0500

    changes

--- ExplorationJam/Assets/UI Code/score.cs
+++ ExplorationJam/Assets/UI Code/score.cs
@@ -8,15 +8,28 @@ public class score : MonoBehaviour
 {
     public TMP_Text moneyText;
 
+    private TMP_Text Component;
+
+    public TMP_FontAsset newFontAsset;
+
 
     int money = 0;
     int total = 300;
 
 
+    void Awake()
+    {
+        Component = GetComponent<TMP_Text>();
+      
+    }
 
     void Start()
     {
-        moneyText.text = "Money  " + "$" + money.ToString() +  " / " + "$" + total.ToString();
+        if (newFontAsset != null && Component != null)
+        {
+            Component.font = newFontAsset;
+        }
+        moneyText.text = "$" + money.ToString() +  " / " + "$" + total.ToString();
     }
 
     // Update is called once per frame
